f282ee155a2b6997a74392f5a1d14545f5645c28
zinobias <zinobias@live.nl>
2022-01-13 13:07:01 +0100

    added everything beside .env

---
+++ get_target_ppp.py
@@ -0,0 +1,145 @@
+from codamconnector import exchange
+import json
+from codamconnector.baseconnector import BaseConnector
+from codamconnector.exchange import Exchange
+from codampystd import List, Object, object
+import codamconnector
+from typing import DefaultDict, Optional
+import requests
+from urllib3 import response
+import sys, getopt
+from collections import Counter, defaultdict
+import re
+import random
+campus_id = 14
+codam_id = 14
+# project_session_id =3300
+# user_id ="nico_codam"
+# project_id =2009
+# project_name="so_long"
+# project_id =1314
+# project_name="libft"
+endpoint_ = codamconnector.IntraConnector(root="https://api.intra.42.fr/v2/")
+
+def get_exchange(request_string, optional = None):
+	exchange = codamconnector.Exchange(
+		url=request_string,
+		body = optional
+	)
+	return exchange
+
+	# "begin_at": f"{begin_at}2017-11-27 09:00:00 UTC",
+def make_scale_team(begin_at = None, scale_id = None, team_id = None, user_id = None):
+	params = {"scale_team" : {
+	"begin_at": f"{begin_at} UTC",
+	"scale_id": f"{scale_id}",
+	"team_id": f"{team_id}",
+	"user_id": f"{user_id}",
+	"comment": "",
+	"answers_attributes": "",
+	"question_id": ""
+	}}
+	return json(params)
+
+def	probe(project_id, campus_id):
+	page_number = 1
+	url_last = None
+	# exchange = get_exchange(f"projects/{project_id}/scale_teams?filter[campus_id]={campus_id}")
+	result_probe = []
+	list_students = []
+	i = 0
+	while 1:
+		exchange = get_exchange(f"projects/{project_id}/scale_teams?filter[campus_id]={campus_id}&page[number]={page_number}&page[size]=100&range[created_at]=2021-01-11,2021-12-17")
+		endpoint_.get(exchange)
+		if len(exchange.result) == 0:
+			break
+	
+		result_probe +=  list(exchange.result)
+		page_number += 1
+		# numbers = re.findall(r"\[([A-Za-z0-9_]+)\]", str(exchange.response))
+		# numbers = re.sub(r'[\[\]!\']', '', str(numbers))
+		# entry.correcteds[0].login == "nico_codam" and 
+		for entry in exchange.result:
+			if str(entry.team.status) != "finished":
+				if hasattr(entry.truant, 'login') == False:
+					# print(entry.truant)
+					# print(entry)
+					# print("\n--------------------------------\n")
+					list_students.append(entry.correcteds[0].login)
+		i += 1
+	return list(set(list_students)), Counter(list_students)
+
+def save_as_json(target_list, project_name=""):
+	with open(f"eval_status_{project_name}.json", "w+") as target_list_f:
+		json.dump(target_list, target_list_f, indent=4)
+	return
+
+# CURSUS_ID = 21
+def parse_projects():
+	projects = defaultdict(dict)
+	with open("project_ids", "r+") as projects_f:
+		lines = projects_f.readlines()
+	for line in lines:
+		project_name, project_id = line.strip().split('=')
+		projects[project_name] = project_id
+	return projects
+
+def make_scaleteam():
+
+	return
+def main_(argv=None, evaluator_intra=0, external=False, project_name=''):
+	try:
+		projects = parse_projects()
+		if external == False:
+			argc = len(sys.argv)
+			if argc != 2:
+				print("Invalid args\nFormat: [project_name]\n")
+				if external == False:
+					exit()
+			# name of the project
+			project_name = sys.argv[1]
+			if not project_name in projects.keys():
+				print("ERROR, INVALID PROJECT_NAME")
+				return(False)
+			project_id = projects[project_name]
+		else:
+			if project_name in projects.keys():
+				project_id = projects[project_name]
+			else:
+				print("ERROR: INVALID PROJECT NAME")
+				return(False)
+		students_list, counter_list_students = probe(project_id, 14)
+		i = 0
+		len_list = len(students_list)
+		target_list = []
+		while (i < len_list):
+			# print(f"Student id : {students_list[i]}\neval count : {counter_list_students[students_list[i]]}")
+			target = {"student_id": students_list[i], "eval_count":counter_list_students[students_list[i]], "project_name":project_name}
+			target_list += [target]
+			i += 1
+		save_as_json(target_list, project_name=project_name)
+		selection_list = []
+		for target in target_list:
+			if target["eval_count"] == 2:
+				selection_list += [target]
+		random_target =	random.choice(selection_list)
+		
+		# CREATE SCALETEAM HERE
+		# evaluator_intra WITH RANDOM_TARGET['student_id']
+		exchange = get_exchange(f"users/{random_target['student_id']}")
+		endpoint_.get(exchange)
+		if external == True:
+			return (True, exchange.result.email)
+		else:
+			print(exchange.result.email, random_target['student_id'])
+			return
+	except:
+		print("INTERNAL ERROR HAS OCCURRED IN : GET_TARGET_PPP")
+		return False
+		#  gets lists of students for specified project.
+		# f.e list["user1", "user2", "user3"]
+		# Counter list students is a dictionary, that gets the amount of occurrences of IDs.
+		# To count the eval count.
+
+if __name__ == '__main__':
+	main_(sys.argv)
--- test.py
+++ test.py
@@ -1,1 +1,209 @@
-print("Hello world OwO")
+from flask.typing import ResponseReturnValue
+import slack
+import os
+import time
+from get_target_ppp import main_
+from pathlib import Path
+from dotenv import load_dotenv
+from flask import Flask, request, Response
+from slackeventsapi import SlackEventAdapter
+from slack.errors import SlackApiError
+from werkzeug.wrappers import response
+from collections import defaultdict
+from datetime import datetime
+import json
+import logging
+# Need to install slackclient w/ respective package manager.
+# Need to install Flask w/ repsective package manager.
+# Need to install slackeventsapi
+# Need to install dotenv & pathlib
+# ngrok for testing.
+# request port is forwarding thingy
+# running on default port 5000
+
+event_endpoint = '/slack/events'
+interactive_endpoint = 'slack/interactive-endpoint'
+# Sets env Path so it knows where to load the environmental variables from.
+env_path = Path('.') / '.env'
+# Load_dotenv loads out env file.
+load_dotenv(dotenv_path=env_path)
+#  Initializes the slack webclient w/ tokens. & authenthicates.
+client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+app = Flask(__name__)
+# Sets the signing secret from the env
+slack_event_adapter = SlackEventAdapter(os.environ['SLACK_SIGNING_SECRET'],event_endpoint, app)
+BOT_ID = client.api_call("auth.test")['user_id']
+
+def parse_projects():
+	projects = defaultdict(dict)
+	with open("project_ids", "r+") as projects_f:
+		lines = projects_f.readlines()
+	for line in lines:
+		project_name, project_id = line.strip().split('=')
+		projects[project_name] = project_id
+	return projects
+
+def add_error_log(e):
+	with open("error_log.txt", "a") as error_log:
+		error_log.write(Response() + '\n' + e)
+
+class user_:
+	def __init__(self, user_data=dict()):
+		self.email = str()
+		self.display_name = str()
+		self.real_name = str()
+		self.user_id = str()
+		self.user_data = user_data
+
+class slack_commands:
+	def __init__(self):
+		self.token = os.environ['SLACK_TOKEN']
+		self.command_list = [["get", "eval", '[PROJECT NAME]'],["help"],["project", "list"]]
+	
+	def	help(self, user=user_()):
+
+		connector.send_private_message(user.user_id, "Possible commands (Case sensitive):"),
+		for command in self.command_list:
+			if command[0] == 'help':
+				continue
+			payload = ''
+			for string in command:
+				time.sleep( ( 1 / 1000000.0)  * 50)
+				payload += string + ' '
+			connector.send_private_message(user.user_id, payload),
+
+	def get_eval(self, display_name, evaluator_id, project_name):
+			response, target_email = main_(evaluator_intra=display_name, external=True, project_name=project_name)
+			if response == False:
+				connector.send_message(text="Something went wrong, or no available candidates.")
+			elif response == True:
+				target_user = connector.create_user(target_email=target_email)
+				# target_user = connector.create_user(target_email="zinobias@gmail.com")
+			# main_(evaluator_intra=display_name, external=True, project_name=project_name)
+			# return
+			
+			# send message to evaluator
+			connector.send_private_message(target_user_id=evaluator_id, text=f"Hi {display_name}, You have an eval booked with: {target_user.display_name}")
+			# send message to target
+			connector.send_private_message(target_user_id=target_user.user_id, text=f"Hey {target_user.display_name}! You have been selected for a p++ eval!\n {display_name}\n with: {target_user.display_name}.\n\n If you run into any issues, please contact the evaluator or the Education assistant!")
+			history_entry = {"evaluator":display_name, "evaluated":target_user.display_name, "booked at":str(datetime.now())}
+			with open("eval_history.json", "r+") as eval_history:
+				try:
+					data = json.load(eval_history)
+					data["eval_history"].append(history_entry)
+					eval_history.seek(0)
+					json.dump(data, eval_history, indent=4)
+				except:
+					eval_history.seek(0)
+					json.dump({"eval_history":[history_entry]}, eval_history)
+	
+	def print_list(self, target_user_id):
+		project_list = parse_projects()
+		connector.send_private_message(target_user_id,text="Hi, following are the available projects:")
+		for project in project_list:
+			connector.send_private_message(target_user_id,text=project)
+			time.sleep( ( 1 / 1000000.0)  * 50)
+
+	def check_project_list(self, msg):
+		p_list = parse_projects()
+		if len(msg) != 3:
+			return False
+		for project in p_list:
+			if msg[2] == project:
+				return True
+		return False
+	
+	def	parse_message(self, text, user_info = None):
+		msg = str(text).split()[1:]
+		user = user_(user_info)
+		user.display_name = user_info['profile']['display_name']
+		user.user_id = user_info['id']
+		if msg in self.command_list or (msg[0] == 'get' and msg[1] == 'eval' and self.check_project_list(msg) == True):
+			if msg == ['help']:
+				self.help(user=user)
+			elif msg[0] == 'get' and msg[1] == 'eval' and self.check_project_list(msg):
+				self.get_eval(user.display_name, user.user_id, msg[2])
+			elif msg == ["project", "list"]:
+				self.print_list(user.user_id)
+		else:
+			connector.send_message(text="Invalid command, try \"help\"")
+
+# Replace general with peerplusplus
+class slack_connector:
+	def __init__(self):
+		self.endpoint = event_endpoint
+		self.env_path = env_path
+		self.client = client
+		self.commands = slack_commands()
+		self.token = os.environ["SLACK_TOKEN"]
+		return
+	
+	def	create_user(self, target_email=str()):
+		user_info__ = client.users_lookupByEmail(email=target_email)
+		new_user = user_(user_data=user_info__)
+		new_user.display_name = user_info__["user"]["profile"]["display_name"]
+		new_user.email = target_email
+		new_user.real_name = user_info__["user"]["real_name"]
+		new_user.user_id = user_info__["user"]["id"]
+		return new_user
+	
+	def	send_message(self, channel='#general', text=''):
+		try:
+			self.client.chat_postMessage(channel=channel, text=str(text))
+		except Exception as e:
+			self.client.chat_postMessage(channel=channel, text=str(text))
+			add_error_log(e)
+		return 
+	def send_private_message(self, target_user_id, text):
+		result = client.conversations_open(users=target_user_id)
+		target_channel_id = result["channel"]["id"]
+		connector.send_message(channel=target_channel_id, text=text)
+		result = client.conversations_close(token=self.token,channel=target_channel_id)
+	
+	def	parse_message(self, text, user_info=None):
+		self.commands.parse_message(text, user_info=user_info)
+
+connector = slack_connector()
+@slack_event_adapter.on('member_joined_channel')
+def	bot_message(payLoad):
+	# set channel to peer++ channel id
+	# peerpp_channel_id = [p++id]
+	
+	user_id = payLoad['event']['user']
+	channel_id = payLoad['event']['channel']
+		
+	# if not channel_id == peerpp_channel_id:
+	# 	return
+	try:
+		result = client.conversations_open(users=user_id)
+		user_channel_id = result["channel"]["id"]
+		connector.send_message(channel=user_channel_id, text="Hi welcome to the peerplusplus channel!\n I am the Bot you need when you want to do a P++eval :D !\n Type @[my_name] following a command in the peerplusplus channel to reach me, try \"help\"")
+		client.conversations_close(token=os.environ["SLACK_TOKEN"], channel=user_channel_id)
+	except Exception as e:
+		add_error_log(e)
+
+@slack_event_adapter.on('app_mention')
+def message(payLoad):
+	event = payLoad.get('event', {})
+	channel_id = event.get('channel')
+	user_id = event.get('user')
+	text = event.get('text')
+	# peer++channel_id = [peer++_channel_id]
+	# TODO : once deployed, set channel_id to peer++ so it will only listen to that channel.
+	# if not channel_id == peerpp_channel_id:
+	# 	return
+	try:
+		response = client.users_info(user = user_id)
+		user_info = response['user']
+		if  user_info['profile']['display_name'] == '':
+			connector.send_message(channel = channel_id, text="Error : [Your account has no valid display_name]")
+		else:
+			connector.parse_message(text, user_info=user_info)
+	except SlackApiError as e:
+		error_message = e.response['error']
+		connector.send_message(channel = channel_id, text=f'Something went wrong :C\n Error: [{error_message}]')
+		add_error_log(e)
+		
+
+if __name__ == "__main__":
+	app.run(debug=True)
