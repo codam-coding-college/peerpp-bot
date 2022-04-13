0117cacc3fd08b4e5a5bfae961d0f129ad49d4ea
SirMorfield <joppe.koers@gmail.com>
2022-04-13 10:45:30 +0200

    refactor: cleanup code

--- get_target_ppp.py
+++ get_target_ppp.py
@@ -14,15 +14,10 @@ import random
 from typing import List, Set, Dict, Tuple, Optional
 from constants import get_projects, CAMPUS_ID
 
-endpoint_ = codamconnector.IntraConnector(root="https://api.intra.42.fr/v2/")
+endpoint = codamconnector.IntraConnector(root="https://api.intra.42.fr/v2/")
 
 
-def get_exchange(request_string, optional=None):
-	exchange = codamconnector.Exchange(url=request_string, body=optional)
-	return exchange
-
-
-	# "begin_at": f"{begin_at}2017-11-27 09:00:00 UTC",
+# "begin_at": f"{begin_at}2017-11-27 09:00:00 UTC",
 def make_scale_team(begin_at=None, scale_id=None, team_id=None, user_id=None):
 	params = {"scale_team": {"begin_at": f"{begin_at} UTC", "scale_id": f"{scale_id}", "team_id": f"{team_id}", "user_id": f"{user_id}", "comment": "", "answers_attributes": "", "question_id": ""}}
 	return json(params)
@@ -36,8 +31,8 @@ def probe(project_id, campus_id):
 	list_students = []
 	i = 0
 	while 1:
-		exchange = get_exchange(f"projects/{project_id}/scale_teams?filter[campus_id]={campus_id}&page[number]={page_number}&page[size]=100&range[created_at]=2021-01-11,2021-12-17")
-		endpoint_.get(exchange)
+		exchange = codamconnector.Exchange(f"projects/{project_id}/scale_teams?filter[campus_id]={campus_id}&page[number]={page_number}&page[size]=100&range[created_at]=2021-01-11,2021-12-17")
+		endpoint.get(exchange)
 		if len(exchange.result) == 0:
 			break
 
@@ -63,63 +58,41 @@ def save_as_json(target_list, project_name=""):
 	return
 
 
-# CURSUS_ID = 21
-
-
-def main_(argv=None, evaluator_intra=0, external=False, project_name=''):
+def main(project_name: str, intra_name: str) -> Tuple[bool, str, str]:
+	projects = get_projects()
 	try:
-		projects = get_projects()
-		if external == False:
-			argc = len(sys.argv)
-			if argc != 2:
-				print("Invalid args\nFormat: [project_name]\n")
-				if external == False:
-					exit()
-			# name of the project
-			project_name = sys.argv[1]
-			if not project_name in projects.keys():
-				print("ERROR, INVALID PROJECT_NAME")
-				return (False)
-			project_id = projects[project_name]
-		else:
-			if project_name in projects.keys():
-				project_id = projects[project_name]
-			else:
-				print("ERROR: INVALID PROJECT NAME")
-				return (False)
-		students_list, counter_list_students = probe(project_id, CAMPUS_ID)
+		students_list, counter_list_students = probe(projects[project_name], CAMPUS_ID)
 		i = 0
-		len_list = len(students_list)
 		target_list = []
-		while (i < len_list):
-			# print(f"Student id : {students_list[i]}\neval count : {counter_list_students[students_list[i]]}")
+		while (i < len(students_list)):
 			target = {"student_id": students_list[i], "eval_count": counter_list_students[students_list[i]], "project_name": project_name}
 			target_list += [target]
 			i += 1
 		save_as_json(target_list, project_name=project_name)
+
 		selection_list = []
 		for target in target_list:
 			if target["eval_count"] == 2:
 				selection_list += [target]
-		random_target = random.choice(selection_list)
 
+		random_target = random.choice(selection_list)
 		# CREATE SCALETEAM HERE
 		# evaluator_intra WITH RANDOM_TARGET['student_id']
-		exchange = get_exchange(f"users/{random_target['student_id']}")
-		endpoint_.get(exchange)
-		if external == True:
-			return (True, exchange.result.email)
-		else:
-			print(exchange.result.email, random_target['student_id'])
-			return
+		exchange = codamconnector.Exchange(f"users/{random_target['student_id']}")
+		endpoint.get(exchange)
+		return True, exchange.result.email, random_target['student_id']
 	except:
 		print("INTERNAL ERROR HAS OCCURRED IN : GET_TARGET_PPP")
-		return False
-		#  gets lists of students for specified project.
-		# f.e list["user1", "user2", "user3"]
-		# Counter list students is a dictionary, that gets the amount of occurrences of IDs.
-		# To count the eval count.
+		return False, '', ''
 
 
 if __name__ == '__main__':
-	main_(sys.argv)
+	if len(sys.argv) != 2:
+		print("Invalid args\nFormat: [project_name]\n")
+		exit()
+	project_name = sys.argv[1]
+	projects = get_projects()
+	if not project_name in projects.keys():
+		print("ERROR, INVALID PROJECT_NAME")
+		exit()
+	main(project_name)
