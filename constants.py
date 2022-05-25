088099b4f7df19175573ec81251f363c9ef335b8
SirMorfield <joppe.koers@gmail.com>
2022-05-25 20:25:00 +0200

    hotfix: remove unused var

--- constants.py
+++ constants.py
@@ -26,5 +26,3 @@ PROJECTS = get_projects()
 # number of seconds to keep the evaluation locks in cache
 # as to not send a (slow) request to intra very time a moderator lists the possible peer++ evals
 GET_EVALUATION_LOCKS_TTL = 5 * 60
-
-PEERPP_SLACKBOT_ID = config('PEERPP_SLACKBOT_ID')
