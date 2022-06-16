cf29add6262ff4628f67f646d9218404c697fa76
SirMorfield <joppe.koers@gmail.com>
2022-06-16 12:47:45 +0200

    backup push

---
+++ src/slack.ts
@@ -0,0 +1,44 @@
+import { App, KnownEventFromType } from '@slack/bolt'
+import { env } from './env'
+import { Intra } from './intra/intra';
+import * as onMessage from './messageParsers';
+import { User, ErrorMsg } from './types'
+
+export const app = new App({
+	signingSecret: env.SIGNING_SECRET,
+	token: env.SLACK_TOKEN,
+	socketMode: true,
+	appToken: env.SLACK_APP_TOKEN,
+})
+
+function isDirect(message: KnownEventFromType<'message'>) {
+	return message.channel === 'D0367EABH28' // TODO this can be done better
+}
+
+app.message(/.*/i, async ({ message, say }) => {
+	if (!isDirect(message))
+		return
+	//@ts-ignore
+	let text: string = message.text
+	if (!text)
+		return
+	text = text.trim().toLowerCase()
+
+	if (text.match(/^help/))
+		onMessage.help(say)
+	else if (text.match(/^list-projects/))
+		onMessage.listProjectIds(say)
+	else if (text.match(/^list-evaluations/))
+		onMessage.listEvaluations(say)
+	else if (text.match(/^book-evaluation/)) {
+		const project: string = text.replace(/^book-evaluation/, '').trim()
+		if (project.length == 0)
+			onMessage.listProjectIds(say)
+		else if (Object.keys(env.projectSlugs).includes(project))
+			onMessage.bookEvaluation(say, text)
+		else
+			say(`project \`${project}\` not recognized, see help for more info`)
+	}
+	else
+		say(`command \`${text}\` not recognized, see help for more info`)
+})
---
+++ src/types.ts
@@ -0,0 +1,9 @@
+
+export type ErrorMsg = string
+export interface User {
+	intraUID: number
+	intraLogin: string
+	url: string
+	email: string
+	slackUID: string
+}
