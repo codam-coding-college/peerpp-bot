a02c3fed226c2a4a0bddbcf6e0010ff02b44acbc
SirMorfield <joppe.koers@gmail.com>
2022-06-28 18:11:34 +0200

    feat: set depth of object expansion in terminal to unlimited

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import { app as slackApp } from './slack/slack'
 import { app as webhookApp } from './webhook/webhook'
+import util from 'util'
+
+// set depth of object expansion in terminal to unlimited
+util.inspect.defaultOptions.depth = null;
 
 (async () => {
 	await slackApp.start()
