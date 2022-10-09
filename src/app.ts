307071494278f8df72c8ca9c7dac576bd6276d97
W2Wizard <63303990+W2Wizard@users.noreply.github.com>
2022-10-10 01:28:54 +0200

    Fix incorrect import name

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import Fast42 from "@codam/fast42";
 import { Database } from 'sqlite3';
 import { Intra } from "./utils/intra/intra";
 import { slackApp } from "./slackbot/slack";
-import { app as webhookApp } from "./webhook/webhook";
+import { webhookApp } from "./webhook/webhook";
 
 // TODO: Go over the entire application and review error messages.
 
