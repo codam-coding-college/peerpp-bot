e6f7acbfbdef0044009d8907b58b1d2ab1801040
W2.Wizard <63303990+W2Wizard@users.noreply.github.com>
2022-10-25 15:12:55 +0200

    Add utility scripts

---
+++ src/scripts/addPoint.js
@@ -0,0 +1,47 @@
+// -----------------------------------------------------------------------------
+// Codam Coding College, Amsterdam @ 2022.
+// See README in the root project for more information.
+// -----------------------------------------------------------------------------
+
+// Script to add a point to a user due to Peer++ point mismanagment.
+
+const fs = require("fs");
+const fast42 = require("@codam/fast42");
+const dotenv = require("dotenv");
+
+//===================================================//
+
+const poolID = 39;
+const args = process.argv.slice(2);
+const env = dotenv.parse(fs.readFileSync("./config/.env"));
+const config = JSON.parse(fs.readFileSync("./config/config.json").toString());
+
+//===================================================//
+
+(async () => {
+	if (args[0] == undefined)
+		return console.error("Invalid usage: <user>");
+
+    const api = await new fast42.default([{
+		client_id: env["INTRA_UID"],
+        client_secret: env["INTRA_SECRET"]
+    }]).init();
+
+    const userResponse = await api.get(`/users/${args[0]}`);
+    if (!userResponse.ok)
+        return console.error(`Failed to fetch user: ${userResponse.statusText}`);
+
+    const userData = await userResponse.json();
+    console.log(`Found user: ${userData.login}`)
+
+    const pointRemResponse = await api.delete(`/pools/${poolID}/points/remove`, { "points": 1 });
+    if (!pointRemResponse.ok)
+        return console.error(`Failed to remove evalpoint from pool: ${pointRemResponse.statusText}`);
+
+    const pointResponse = await api.post(`/users/${userData.id}/correction_points/add`, {
+		"reason": "Manual point correction due to Peer++."
+	});
+    if (!pointResponse.ok)
+        return console.error(`Failed to give point to user: ${pointResponse.statusText}`);
+    console.log("Point given!");
+})();
---
+++ src/scripts/getUsers.js
@@ -0,0 +1,52 @@
+// -----------------------------------------------------------------------------
+// Codam Coding College, Amsterdam @ 2022.
+// See README in the root project for more information.
+// -----------------------------------------------------------------------------
+
+// Script to fetch all current users in the specified group id in the config.
+
+const fs = require("fs");
+const fast42 = require("@codam/fast42");
+const dotenv = require("dotenv");
+
+//===================================================//
+
+const env = dotenv.parse(fs.readFileSync("./config/.env"));
+const config = JSON.parse(fs.readFileSync("./config/config.json").toString());
+
+//===================================================//
+
+(async () => {
+    const api = await new fast42.default([{
+		client_id: env["INTRA_UID"],
+        client_secret: env["INTRA_SECRET"]
+    }]).init();
+
+    const groupsResponse = await api.getAllPages(`/groups/${config.groupID}/groups_users`);
+    await Promise.all(groupsResponse).catch((reason) => {
+        return console.error(`Failed to get groups: ${reason}`);
+    });
+
+    const users = [];
+    for await (const page of groupsResponse) {
+        if (!page.ok)
+            return console.error(`Failed to fetch group users: ${page.statusText}`);
+        const groupUsers = await page.json();
+        
+        for (const user of groupUsers)
+            users.push(user.user_id);
+    }
+
+    const userResponse = await api.getAllPages(`/users`, {
+        "filter[id]": users.join(',') 
+    });
+
+    for await (const page of userResponse) {
+        if (!page.ok)
+            return console.error(`Failed to fetch user: ${page.statusText}`);
+        const usersPage = await page.json();
+        
+        for (const user of usersPage)
+            console.log(user.login)
+    }
+})();
