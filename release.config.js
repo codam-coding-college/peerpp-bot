e0e391631797bb9364cd12ed76b777fdcfe75a94
Freek Bes <freek.bes@codam.nl>
2026-08-04 14:01:35 +0200

    Add container image deploy workflow

---
+++ release.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+	branches: ["main"],
+	plugins: [
+		"@semantic-release/commit-analyzer",
+		"@semantic-release/release-notes-generator",
+		[
+			"@semantic-release-plus/docker",
+			{
+				name: "ghcr.io/codam-coding-college/peerpp-bot",
+				skipLogin: true,
+			},
+		],
+		[
+			"@semantic-release/npm",
+			{
+				npmPublish: false
+			}
+		],
+		"@semantic-release/github",
+		[
+			"@semantic-release/git",
+			{
+				assets: ["package.json"],
+				message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+			}
+		]
+	]
+};
